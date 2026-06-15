55cfde08d1178c2bb615429564c54fbbf7279c92
Hemanth Sai Anisetti <hemanthsaianisetty5@gmail.com>
2026-06-15 16:12:50 +0530

    Create script.js

---
+++ frontend/script.js
@@ -0,0 +1,4 @@
+document.getElementById("uploadBtn").addEventListener("click", () => {
+    document.getElementById("result").innerHTML =
+        "<h3>Resume Upload Module Coming Soon...</h3>";
+});
